7b49c197ef7e755b29ec839eed4b118f9c7c6a88
Drewsome <cyclingdrew@gmail.com>
2026-05-04 07:17:23 -0700

    Update failure.js

--- js/failure.js
+++ js/failure.js
@@ -1,5 +1,6 @@
 function detectFailures(){
+
   Object.values(STATE.nodes).forEach(n=>{
-    n.failed = n.effectiveVoltage < 11;
+    n.failed = n.effectiveVoltage < 12; // raise threshold
   });
 }
